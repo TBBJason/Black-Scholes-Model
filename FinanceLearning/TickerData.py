e702d65e9db7be2fac75aaaf280f7d671b18eb2e
John Doe <john.doe@example.com>
2024-11-03 17:27:57 -0500

    from laptop

--- FinanceLearning/TickerData.py
+++ FinanceLearning/TickerData.py
@@ -3,17 +3,19 @@ import yfinance as yf
 class ticker_data:
     def __init__(self, ticker):
         self.ticker = ticker.upper()
-
-        self.information = self.information = yf.Ticker(self.ticker)
+        self.information = yf.Ticker(self.ticker)
     def current_close(self):
-        current = self.information.history(period='1d')['Close'][0]
+        current = self.information.history(period='1y')['Close']
         return current
     def columns(self):
-        return 
+        columns = self.information.columns
+        return columns
+
 
 
 
 if __name__ == "__main__":
     aapl = ticker_data('aapl')
+    columns = aapl.columns
     current = aapl.current_close()
-    print(current)
+    print(current, columns)
